9c4e8089cc3f07b3681eb7141b772cefd5378103
Rohit Vishwakarma <128726730+RohitVishwakarma8840@users.noreply.github.com>
2024-09-04 16:41:14 +0530

    Add files via upload

---
+++ src/components/Admin/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Admin =()=> {
+  return (
+    <div>
+
+<aside className='w-[280px] bg-indigo-600 fixed top-0 left-0 h-full'></aside>
+  <section className='bg-gray-100 h-screen '
+  style={{
+    marginLeft : 280
+  }}
+  >
+ 
+  <nav className='bg-white p-8 shadow'>
+<h1>Coding ott</h1>
+  </nav>
+  </section>
+
+    </div>
+  )
+}
+
+export default Admin
